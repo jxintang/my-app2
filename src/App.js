4f4e9ee957676699dd33cbcf345c2dbcb4c60a63
tangjingxin <tangjingxin@zrcaifu.com>
2017-08-03 11:30:05 +0800

    hehe

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to master-2,lala</h2>
+          <h2>Welcome to React,hehe</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
