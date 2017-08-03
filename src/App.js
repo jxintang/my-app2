da8a837b1d06c34a9619c81f02e7c7e140d96ccc
tangjingxin <tangjingxin@zrcaifu.com>
2017-08-03 11:35:35 +0800

    my-work-1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to master-2,lala</h2>
-          <h2>Welcome to React,hehe</h2>
+          <h2>Welcome to React,lala</h2>
+          <h2>Welcome to my-work,lala</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
